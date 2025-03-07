94f1b7455553afeac80ef4db8f8cfcc5c9538dc1
SimonasGer <159446055+SimonasGer@users.noreply.github.com>
2025-03-07 17:45:09 +0200

    final?

--- content.js
+++ content.js
@@ -1,5 +1,16 @@
-if (localStorage.getItem("autoRefresh") === "true") {
-    setTimeout(() => {
-        location.reload();
-    }, localStorage.getItem("time") * 1000);
-}
+chrome.storage.local.get(["autoRefresh", "time"], (data) => {
+    if (data.autoRefresh === "true" && data.time) {
+        setTimeout(() => {
+            location.reload()
+        }, data.time * 1000)
+    }
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    chrome.storage.local.get("time", (data) => {
+        if (data.time) {
+            document.getElementById("time").value = data.time
+        }
+    })
+})
+
--- popup.js
+++ popup.js
@@ -1,24 +1,41 @@
 document.getElementById("start").addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
     let time = document.getElementById("time").value
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: startRefreshing,
-        args: [time]
-    })
+
+    if (time > 0) {
+        alert("Refreshing Started")
+        chrome.storage.local.set({ autoRefresh: "true", time: time })
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: startRefreshing,
+            args: [time]
+        })
+    } else {
+        alert("Refresh time cannot be zero, negative, or null")
+    }
 })
 
 document.getElementById("stop").addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+    chrome.storage.local.remove(["autoRefresh", "time"])
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         func: stopRefreshing
     })
 })
 
+document.addEventListener("DOMContentLoaded", () => {
+    chrome.storage.local.get("time", (data) => {
+        if (data.time) {
+            document.getElementById("time").value = data.time
+        }
+    })
+})
+
 function startRefreshing(time){
     localStorage.setItem("autoRefresh", "true")
     localStorage.setItem("time", time)
+
     if (!window.refreshInterval) {
         window.refreshInterval = setInterval(() => {
             location.reload()
@@ -27,8 +44,10 @@ function startRefreshing(time){
 }
 
 function stopRefreshing(){
+    alert("Refreshing Stopped")
     localStorage.removeItem("autoRefresh")
     localStorage.removeItem("time")
     clearInterval(window.refreshInterval)
     window.refreshInterval = null
+    location.reload()
 }
